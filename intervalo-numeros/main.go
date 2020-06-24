8abad6f76ddf32221372c1c9e33c631c76f6fe29
samuelssantos <samuelsantos.sjdr@gmail.com>
2020-06-24 15:08:25 -0300

    Incluir suporte a input de uma lista não ordenada

--- intervalo-numeros/main.go
+++ intervalo-numeros/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"strconv"
 	"strings"
 )
+
+// Input list of itens
+type Input []string
 
 const listSeparator = ", "
 
@@ -12,57 +16,74 @@ func getIntervals(intervals []string) string {
 	return strings.Join(intervals, listSeparator)
 }
 
-func madeList(value string) []string {
+func makeList(value string) Input {
 	return strings.SplitN(value, listSeparator, -1)
 }
 
-func getInterval(min int, max int) string {
+func (b *Input) makeOrderedIntList() ([]int, error) {
 
-	start := strconv.Itoa(min)
-	end := strconv.Itoa(max)
-	interval := "[" + start + "-" + end + "]"
-	if start == end {
-		interval = "[" + end + "]"
+	stack := make([]int, len((*b)))
+
+	for index, element := range *b {
+		var value int
+		var err error
+		if value, err = strconv.Atoi(element); err != nil {
+			return nil, err
+		}
+
+		stack[index] = value
 	}
 
+	return stack, nil
+}
+
+func getInterval(min int, max int) string {
+	interval := fmt.Sprintf("[%d-%d]", min, max)
+	if min == max {
+		interval = fmt.Sprintf("[%d]", max)
+	}
 	return interval
 }
 
-func groupBy(stack []int, group *[]string) {
-
-	sort.Ints(stack)
-
-	min := stack[0]
-	max := stack[len(stack)-1]
-	interval := getInterval(min, max)
-	*group = append(*group, interval)
-
+func sortGroup(group []int) (min int, max int) {
+	sort.Ints(group)
+	return group[0], group[len(group)-1]
 }
 
-func agrupar(value string) (string, error) {
+func makeInterval(stack []int, group *[]string) {
+	interval := getInterval(sortGroup(stack))
+	*group = append(*group, interval)
+}
 
-	list := madeList(value)
-	var stack = []int{}
+func doProcessar(value string) (string, error) {
+
+	input := makeList(value)
+	var list []int
+	var err error
+	if list, err = input.makeOrderedIntList(); err != nil {
+		return "", err
+	}
+
+	stack := []int{}
 	var result = []string{}
 	for index, element := range list {
-		j, _ := strconv.Atoi(element)
 		if index == 0 {
-			stack = append(stack, j)
+			stack = append(stack, element)
 		} else {
-			old, _ := strconv.Atoi(list[index-1])
-			if j-1 == old {
-				stack = append(stack, j)
+			old := list[index-1]
+			if element-1 == old {
+				stack = append(stack, element)
 				if index == len(list)-1 {
-					groupBy(stack, &result)
+					makeInterval(stack, &result)
 				}
 				continue
 			}
 
-			groupBy(stack, &result)
+			makeInterval(stack, &result)
 			stack = nil
-			stack = append(stack, j)
+			stack = append(stack, element)
 			if index == len(list)-1 {
-				groupBy(stack, &result)
+				makeInterval(stack, &result)
 			}
 		}
 	}
--- intervalo-numeros/main_test.go
+++ intervalo-numeros/main_test.go
@@ -8,7 +8,7 @@ func TestMain(t *testing.T) {
 
 	t.Run("When_Input_{100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150}_Then_Return_[100-105], [110-111], [113-115], [150]", func(t *testing.T) {
 		var expected = "[100-105], [110-111], [113-115], [150]"
-		if result, err := agrupar("100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150"); err != nil {
+		if result, err := doProcessar("100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150"); err != nil {
 		} else {
 			if result != expected {
 				t.Error(
@@ -17,12 +17,11 @@ func TestMain(t *testing.T) {
 				)
 			}
 		}
-
 	})
 
 	t.Run("When_Input_{0, 1, 2, 3, 4, 5, 10, 11, 13, 14, 15, 17}_Then_Return_[0-5], [10-11], [13-15], [17]", func(t *testing.T) {
 		var expected = "[0-5], [10-11], [13-15], [17]"
-		if result, err := agrupar("0, 1, 2, 3, 4, 5, 10, 11, 13, 14, 15, 17"); err != nil {
+		if result, err := doProcessar("0, 1, 2, 3, 4, 5, 10, 11, 13, 14, 15, 17"); err != nil {
 		} else {
 			if result != expected {
 				t.Error(
@@ -36,7 +35,21 @@ func TestMain(t *testing.T) {
 
 	t.Run("When_Input_{0, 1, 2, 3, 4, 5}_Then_Return_[0-5]", func(t *testing.T) {
 		var expected = "[0-5]"
-		if result, err := agrupar("0, 1, 2, 3, 4, 5"); err != nil {
+		if result, err := doProcessar("0, 1, 2, 3, 4, 5"); err != nil {
+		} else {
+			if result != expected {
+				t.Error(
+					"expected", expected,
+					"got", result,
+				)
+			}
+		}
+
+	})
+
+	t.Run("When_UnSorted_Input_{0, 1, 5, 3, 2, 5}_Then_Return_[0-5]", func(t *testing.T) {
+		var expected = "[0-5]"
+		if result, err := doProcessar("0, 1, 2, 3, 4, 5"); err != nil {
 		} else {
 			if result != expected {
 				t.Error(
